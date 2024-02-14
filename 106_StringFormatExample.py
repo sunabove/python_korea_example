5bdb2bd34926eae5d3b22006f88a4e2ef4cfc03d
sunabove <sunabove@nate.com>
2024-02-14 21:03:37 +0900

    **

--- 106_StringFormatExample.py
+++ 106_StringFormatExample.py
@@ -6,11 +6,11 @@ item = "Apple"
 unit_price = 2345.744
 count = 1234
 
-print( item + "price : " + str( unit_price ) + " * " + str( count ) + " = " + str(unit_price*count)  )
+print( item + " price : " + str( unit_price ) + " * " + str( count ) + " = " + str(unit_price*count)  )
 
 # % string formatting
 
-print( "% string formatting" )
+print( "\n% string formatting" )
 
 item = "Apple" 
 unit_price = 2345.744
@@ -20,7 +20,7 @@ print( "%s price : %.2f * %d = %.1f" % ( item, unit_price, count,  unit_price*co
 
 # format() string formatting
 
-print( "format() string formatting" )
+print( "\nformat() string formatting" )
 
 item = "Apple"
 unit_price = 2345.744
@@ -30,10 +30,12 @@ print( "{0} price : {1:,.2f} * {2:,} = {3:,.1f}".format( item, unit_price, count
 
 # fstring formatting
 
-print( "fstring formatting" )
+print( "\nfstring formatting" )
 
 item = "Apple" 
 unit_price = 2345.744
 count = 1234
 
 print( f"{item} price : {unit_price:,.2f} * {count:,} = {unit_price*count:,.1f}" )
+
+print()
