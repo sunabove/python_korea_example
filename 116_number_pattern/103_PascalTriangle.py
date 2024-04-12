7850eb880b802569fa4a2255f164c7932667a38e
sunabove <sunabove@nate.com>
2024-04-12 10:01:07 +0900

    **

--- 116_number_pattern/103_PascalTriangle.py
+++ 116_number_pattern/103_PascalTriangle.py
@@ -1,16 +1,24 @@
 n = [ 0, 1, 0 ]
 
-for i in range( 4 ) :
+for i in range( 10 ) :
 
     t = ""
     for k in n[ 1 : -1 ] :
-        t += f"{k} "
+        t += f"{k:^3} "
     pass
 
-    print( f"{t:^20}" )
+    print( f"{t:^40}" )
 
-    for k in range( len( n ) - 2 ) :
-        n.insert( k + 1, n[k] + n[k+1] )
+    m = [ ]
+
+    m.append( n[0] )
+
+    for k in range( len(n) - 1 ) :
+        m.append( n[k] + n[k+1] )
     pass
+
+    m.append( n[-1] )
+
+    n = m 
 
 pass
