f67274c184a092f9e73b3971dce829e118ba014d
sunabove <sunabove@nate.com>
2024-04-12 11:26:03 +0900

    **

---
+++ 116_number_pattern/204_SameNumberGenerator.py
@@ -0,0 +1,7 @@
+a = 12345679
+
+for i in range( 9, 82, 9 ) :
+
+    print( f"{a} * {i:>2} = {a*i:,}" )
+
+pass
---
+++ 116_number_pattern/206_PyramidGenerator.py
@@ -0,0 +1,9 @@
+for i in range( 7, -3, -1 ) :
+
+    a = 0
+    for k in range( 9, i + 1, -1 ) :
+        a = a*10 + k
+
+    print( f"{a:>10} * 9 {['-', '+'][ i > -1 ]} {abs(i)} = {a*9 + i}" )
+
+pass
