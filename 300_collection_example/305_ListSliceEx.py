ab1d8859fc903a8ebd29775af083baffaa1b5d07
sunabove <sunabove@nate.com>
2024-05-10 12:28:41 +0900

    Create 305_ListSliceEx.py

---
+++ 300_collection_example/305_ListSliceEx.py
@@ -0,0 +1,10 @@
+# list slice example
+list = [ 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 ]
+
+print( 0, list )
+print( 1, list[ 2:8:2 ] )
+print( 2, list[ 0:10:2 ] )
+print( 3, list[ ::2 ] )
+print( 4, list[ ::-2 ] )
+print( 5, list[ :: ] )
+print( 6, list[ ::-1] )
